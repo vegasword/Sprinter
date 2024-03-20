6ea3d304e1e15ddcf02e1e7a9a27def50fc47f1d
Alexandre Perché <a.perche.dev@gmail.com>
2024-03-20 17:20:43 +0100

    Sign in query, restructured client folders

---
+++ client/src/signin.ts
@@ -0,0 +1,19 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
+const form = document.getElementById("signin-form");
+
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email") as HTMLInputElement;
+  const password = document.getElementById("password") as HTMLInputElement;
+
+  if (email !== undefined || password !== undefined) {
+    socket.emit("user:signin", email.value, password.value);
+  }  
+});
+
+socket.on("user:signin:success", () => window.location.href="about:blank");
+
