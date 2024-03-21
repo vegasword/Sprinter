07202f5a21c07122ae841b65b630dfadba92e09a
Alexandre Perché <a.perche.dev@gmail.com>
2024-03-22 00:02:28 +0100

    Updated sign in page and made a two custom themes

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,6 +5,25 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["corporate", "business"]
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          "primary": "#9f338a",
+          "secondary": "#6D2D61",
+          "accent": "#e30253",
+          "neutral": "#9f338a",
+          "base-100": "#ffffff",
+        },
+        business: {
+          ...require("daisyui/src/theming/themes")["business"],
+          "primary": "#9f338a",
+          "secondary": "#6D2D61",
+          "accent": "#e30253",
+          "neutral": "#9f338a",
+          "base-100": "#ffffff",
+        }
+      }
+    ]
   }
 }
