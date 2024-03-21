b54aa3bcfd02cee7efc0390b49e0925c74e515a7
Alexandre Perché <a.perche.dev@gmail.com>
2024-03-21 17:29:01 +0100

    Migrated style to Tailwind + daisyUI, restructured again client folders and files

--- client/src/signin.ts
+++ client/src/signin.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000");
 
-const form = document.getElementById("signin-form");
+const form = document.getElementById("sign-in");
 
 form?.addEventListener("submit", (event) => {
   event.preventDefault();
